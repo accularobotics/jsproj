d6c7d81608db62d8c369b986247aab863799bdba
accularobotics <accula.robotics@gmail.com>
2024-09-11 17:19:09 -0300

    v1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 function mostrarNome() {
     let nomeUsuario = document.getElementById("nomeUsuario").value
-    document.getElementById("paragrafo").innerText = "bem vindo "  + nomeUsuario
+    document.getElementById("paragrafo").innerText = "Bem vindo," + nomeUsuario
+}
+
+function calcAreaCirculo(){
+    let raio= document.getElementById("raio").value
+    const pi = 3.14
+    let area= pi*raio**2
+    document.getElementById("area").innerText = area
 }
