9ce241f97fa30f6acef1c0034ce22bc45fbbc18a
accularobotics <accula.robotics@gmail.com>
2024-09-11 17:31:21 -0300

    v2

--- js/script.js
+++ js/script.js
@@ -9,3 +9,14 @@ function calcAreaCirculo(){
     let area= pi*raio**2
     document.getElementById("area").innerText = area
 }
+
+function Soma() {
+
+    let n1 = Number(document.getElementById("n1").value)
+
+    let n2 = Number(document.getElementById("n2").value)
+    let soma = n1 + n2
+    document.getElementById("soma").innerText = soma
+
+
+}
