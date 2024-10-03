3ff6e181b5eea0429f6da538687cb87945968d5f
accularobotics <accula.robotics@gmail.com>
2024-10-03 14:15:47 -0300

    piloto

--- js/script.js
+++ js/script.js
@@ -14,50 +14,42 @@ function Tabuada() {
 
     document.getElementById("x1").innerText = a; // Atualiza o conteúdo de x1
 }
-// ---------------------------
-function somaProdutos() {
-    let p1 = Number(document.getElementById("produto1").value);
-    let p2 = Number(document.getElementById("produto2").value);
-    let p3 = Number(document.getElementById("produto3").value);
-    let p4 = Number(document.getElementById("produto4").value);
-    let p5 = Number(document.getElementById("produto5").value);
+function clientes() {
+
+    let cliente = Number(document.getElementById("clientes").value);
+    let capac = Number(document.getElementById("capacidade").value);
+
+
+
+ }
+
+ let capac = 1
+ function capacidade() {
+
+    let cliente = Number(document.getElementById("clientes").value);
+    // let capac = Number(document.getElementById("capacidade").value);
     
-    let resultadoy;
-    let px = '';
 
-    // Limpa o conteúdo anterior
-    document.getElementById("r1").innerText = '';
-    resultadoy = p1 + p2 + p3 + p4 + p5 ;
+    while (cliente <= capac){
+        capac -= capac
+        if(capac==0) {
+            document.getElementById("cap").innerText = "Restaurante Lotado, não há mais mesas disponiveis"; // Atualiza o conteúdo de x1
+            document.getElementById("cap").disabled = true
+        } else {document.getElementById("cap").innerText = "Temos vagas" }
+    }
 
-    // for (let pr = 1; pr <= 5; pr++) {
-    //     resultadoy = p1 + p2 + p3 + p4 + p5 ;
-    //     // p1 += `${px} r ${pr} = ${resultadoy}\n`; // Formata a string
-    // }
+        
 
-    document.getElementById("r1").innerText = resultadoy; // Atualiza o conteúdo de x1
-}
-// ---------------------------
+ }
+
+ function clientes() {
+    let produtosS = Number(document.getElementById("valorProdutos").value);
+    let Somados
+
+    while (produtosS <= Somados){
 
 
 
-let saldoAtual = 0;
 
-function extratos() {
-    // Obtém o saldo inicial e o valor da operação
-    const saldoInicial = Number(document.getElementById("saldo").value);
-    const operacao = Number(document.getElementById("operacao").value);
-    const tipoOperacao = document.getElementById("tipo").value;
-    if (tipoOperacao != "d/c") {
-            // Verifica o tipo de operação e atualiza o saldo
-            if (tipoOperacao === "+") {
-                saldoAtual = saldoInicial + operacao; // Depósito
-            } else if (tipoOperacao === "-") {
-                saldoAtual = saldoInicial - operacao; // Saque
-            }
 
-            // Atualiza o campo de saldo inicial e o texto de saldo atual
-            document.getElementById("saldo").value = saldoAtual;
-            document.getElementById("SaldoAtu").innerText = `Saldo Atual: ${saldoAtual}`;
-            document.getElementById("tipo").value = "d/c"
-        }        
-}
+ }
