f7894436ffa9f30d2f7d99161d706da53d2fa267
accularobotics <accula.robotics@gmail.com>
2024-10-03 17:15:54 -0300

    v1

--- js/script.js
+++ js/script.js
@@ -1,55 +1,46 @@
 
-function Tabuada() {
-    let h = Number(document.getElementById("tabuada").value);
-    let resultados;
-    let a = '';
 
-    // Limpa o conteúdo anterior
-    document.getElementById("x1").innerText = '';
+ let alunos=[]
+ function cadastar() {
 
-    for (let y = 1; y <= 10; y++) {
-        resultados = h * y;
-        a += `${h} x ${y} = ${resultados}\n`; // Formata a string
-    }
-
-    document.getElementById("x1").innerText = a; // Atualiza o conteúdo de x1
-}
-function clientes() {
-
-    let cliente = Number(document.getElementById("clientes").value);
-    let capac = Number(document.getElementById("capacidade").value);
-
-
-
- }
-
- let capac = 1
- function capacidade() {
-
-    let cliente = Number(document.getElementById("clientes").value);
-    // let capac = Number(document.getElementById("capacidade").value);
+    let aluno = document.getElementById("nome").value
+    //console.log(aluno)
+    
+    alunos.push (aluno)
+    let ponteiro = alunos.length 
+    //ponteiro ++
+    document.getElementById("dani").innerHTML += `<p >Aluno :${aluno} N# ${ponteiro}\n</p>`
+    // document.getElementById("capacid").innerText = `${ponteiro}`
     
 
-    while (cliente <= capac){
-        capac -= capac
-        if(capac==0) {
-            document.getElementById("cap").innerText = "Restaurante Lotado, não há mais mesas disponiveis"; // Atualiza o conteúdo de x1
-            document.getElementById("cap").disabled = true
-        } else {document.getElementById("cap").innerText = "Temos vagas" }
+    if (ponteiro >= 10){
+
+        document.getElementById("dani").innerText = "Exede capacidade da turma" 
+        document.getElementById("unico").disabled = true
+   
     }
 
-        
-
- }
-
- function clientes() {
-    let produtosS = Number(document.getElementById("valorProdutos").value);
-    let Somados
-
-    while (produtosS <= Somados){
-
-
-
 
 
  }
+// =================================
+ function createAluno() {
+    if(alunos.length < 3) {
+        let nome = document.getElementById("nome").value
+        let dataNasc = new Date(document.getElementById("dataNasc").value + "T00:00:00")
+        let curso = document.getElementById("curso").value
+        let aluno = {nome: nome, dataNasc: dataNasc, curso: curso}
+        alunos.push(aluno)
+        console.log(alunos)
+    }
+    else {
+        document.getElementById("cadastrarAluno").disabled = true
+        document.getElementById("msg").innerText = "Turma lotada!"
+    }
+}
+
+function readAlunos() {
+    for (i = 0; i < alunos.length; i++) {
+        document.getElementById("alunosCadastrados").innerHTML += `<p>${alunos[i].nome} - ${alunos[i].dataNasc} - ${alunos[i].curso}</p>` // mostra valor digitado pelo usuário na tela
+    }
+}
